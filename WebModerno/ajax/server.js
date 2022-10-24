db32a6b074c0ce8f5011f25dc733dd72826fc95b
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-10-24 17:55:28 -0300

    axios

--- WebModerno/ajax/server.js
+++ WebModerno/ajax/server.js
@@ -6,5 +6,36 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('ok'))
+app.get('/teste', (req, res) => res.send('OK'))
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './upload') //Define uma pasta
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${date.now()}_${file.originalname}`) //Define o nome do arquivo
+  }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      return res.end('Ocorreu um erro.')
+    }
+
+    res.end('Recebido com sucesso.')
+  })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
 app.listen(5500, () => console.log('Executando'))
